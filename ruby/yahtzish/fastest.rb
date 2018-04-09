05e879f20acc43b3a3b7f7d1d51915c18eb56163
Kyrre Havik Eriksen <kyrremann@gmail.com>
2018-04-09 11:17:09 +0200

    rubocop auto correct

--- ruby/yahtzish/fastest.rb
+++ ruby/yahtzish/fastest.rb
@@ -1,16 +1,15 @@
-times = ARGV[0] || 100000
+times = ARGV.first || 100_000
 
 @die = Random.new
 sum = 0
-start = Time.now
-(1..times).each do |n|
+(1..times).each do |_n|
   score = Array.new(6)
-  (1..6).each do |r|
+  (1..6).each do |_r|
     # first roll
-    pool = Array.new(6).map{|d| @die.rand(6) +1}.sort.group_by(&:itself).values.sort{|a,b|a.size <=> b.size}
+    pool = Array.new(6).map { |_d| @die.rand(6) + 1 }.sort.group_by(&:itself).values.sort_by(&:size)
     kv = nil
     (pool.size - 1).downto(0) do |i|
-      if not pool[i].empty? and score[pool[i].first - 1].nil?
+      if !pool[i].empty? && score[pool[i].first - 1].nil?
         kv = i
         break
       end
@@ -18,18 +17,16 @@ start = Time.now
     keep = kv.nil? ? [] : pool.delete_at(kv)
     keep = keep.flatten
     # second roll
-    pool = pool.map{|d| @die.rand(pool.size) +1}
-    more_keep, pool = pool.partition{|d| d == keep.first}
+    pool = pool.map { |_d| @die.rand(pool.size) + 1 }
+    more_keep, pool = pool.partition { |d| d == keep.first }
     keep.append(more_keep).flatten!
     # third roll
-    pool = pool.map{|d| @die.rand(pool.size) +1}
-    more_keep, pool = pool.partition{|d| d == keep.first}
+    pool = pool.map { |_d| @die.rand(pool.size) + 1 }
+    more_keep, pool = pool.partition { |d| d == keep.first }
     keep.append(more_keep).flatten!
     score[keep.first - 1] = keep.sum unless keep.empty?
   end
   sum += score.compact.sum
 end
-
-p Time.now - start
 
 p sum / times
