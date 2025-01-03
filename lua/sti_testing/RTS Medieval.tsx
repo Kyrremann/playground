6da26170bc0a6b7b55159ed3ea4422b28528374a
Kyrre Havik <kyrremann@gmail.com>
2025-01-03 20:55:37 +0100

    feat: bedre navn, og vektet landskapet

--- lua/sti_testing/RTS Medieval.tsx
+++ lua/sti_testing/RTS Medieval.tsx
@@ -1,4 +1,274 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="RTS Medieval" tilewidth="64" tileheight="64" spacing="32" margin="32" tilecount="126" columns="18">
+<tileset version="1.10" tiledversion="1.11.0" name="RTS Medieval" tilewidth="64" tileheight="64" spacing="32" margin="32" tilecount="126" columns="18">
  <image source="medieval_tilesheet.png" width="1760" height="704"/>
+ <tile id="0">
+  <properties>
+   <property name="weight" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="weight" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="weight" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="weight" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="weight" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="weight" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="weight" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="weight" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="weight" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="weight" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="weight" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="weight" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="weight" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="weight" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="weight" type="int" value="7"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="weight" type="int" value="7"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="weight" type="int" value="10"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="weight" type="int" value="5"/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="weight" type="int" value="7"/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <properties>
+   <property name="weight" type="int" value="7"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="weight" type="int" value="10"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="weight" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="weight" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="weight" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="93">
+  <properties>
+   <property name="weight" type="int" value="4"/>
+  </properties>
+ </tile>
 </tileset>
---
+++ lua/sti_testing/rts-medieval.lua
@@ -0,0 +1,535 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.11.0",
+  class = "",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 20,
+  height = 20,
+  tilewidth = 64,
+  tileheight = 64,
+  nextlayerid = 5,
+  nextobjectid = 3,
+  properties = {},
+  tilesets = {
+    {
+      name = "RTS Medieval",
+      firstgid = 1,
+      class = "",
+      tilewidth = 64,
+      tileheight = 64,
+      spacing = 32,
+      margin = 32,
+      columns = 18,
+      image = "medieval_tilesheet.png",
+      imagewidth = 1760,
+      imageheight = 704,
+      objectalignment = "unspecified",
+      tilerendersize = "tile",
+      fillmode = "stretch",
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 64,
+        height = 64
+      },
+      properties = {},
+      wangsets = {},
+      tilecount = 126,
+      tiles = {
+        {
+          id = 0,
+          properties = {
+            ["weight"] = 3
+          }
+        },
+        {
+          id = 1,
+          properties = {
+            ["weight"] = 3
+          }
+        },
+        {
+          id = 2,
+          properties = {
+            ["weight"] = 3
+          }
+        },
+        {
+          id = 3,
+          properties = {
+            ["weight"] = 3
+          }
+        },
+        {
+          id = 4,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 5,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 6,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 7,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 8,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 9,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 10,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 11,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 12,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 13,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 18,
+          properties = {
+            ["weight"] = 3
+          }
+        },
+        {
+          id = 19,
+          properties = {
+            ["weight"] = 3
+          }
+        },
+        {
+          id = 20,
+          properties = {
+            ["weight"] = 3
+          }
+        },
+        {
+          id = 21,
+          properties = {
+            ["weight"] = 3
+          }
+        },
+        {
+          id = 22,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 23,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 24,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 25,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 26,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 27,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 28,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 29,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 30,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 31,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 36,
+          properties = {
+            ["weight"] = 3
+          }
+        },
+        {
+          id = 37,
+          properties = {
+            ["weight"] = 3
+          }
+        },
+        {
+          id = 38,
+          properties = {
+            ["weight"] = 3
+          }
+        },
+        {
+          id = 39,
+          properties = {
+            ["weight"] = 3
+          }
+        },
+        {
+          id = 40,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 41,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 42,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 43,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 44,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 45,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 46,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 47,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 48,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 49,
+          properties = {
+            ["weight"] = 1
+          }
+        },
+        {
+          id = 54,
+          properties = {
+            ["weight"] = 5
+          }
+        },
+        {
+          id = 55,
+          properties = {
+            ["weight"] = 7
+          }
+        },
+        {
+          id = 56,
+          properties = {
+            ["weight"] = 7
+          }
+        },
+        {
+          id = 57,
+          properties = {
+            ["weight"] = 10
+          }
+        },
+        {
+          id = 72,
+          properties = {
+            ["weight"] = 5
+          }
+        },
+        {
+          id = 73,
+          properties = {
+            ["weight"] = 7
+          }
+        },
+        {
+          id = 74,
+          properties = {
+            ["weight"] = 7
+          }
+        },
+        {
+          id = 75,
+          properties = {
+            ["weight"] = 10
+          }
+        },
+        {
+          id = 90,
+          properties = {
+            ["weight"] = 4
+          }
+        },
+        {
+          id = 91,
+          properties = {
+            ["weight"] = 4
+          }
+        },
+        {
+          id = 92,
+          properties = {
+            ["weight"] = 4
+          }
+        },
+        {
+          id = 93,
+          properties = {
+            ["weight"] = 4
+          }
+        }
+      }
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 20,
+      height = 20,
+      id = 1,
+      name = "ground",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+        58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 1, 1, 55, 55, 57, 57, 58, 58,
+        58, 58, 1, 1, 1, 56, 56, 58, 58, 58, 58, 1, 1, 1, 55, 55, 55, 57, 58, 58,
+        58, 58, 58, 58, 1, 1, 56, 56, 56, 56, 58, 1, 1, 1, 1, 1, 55, 57, 57, 58,
+        58, 58, 58, 1, 1, 1, 1, 1, 56, 56, 1, 1, 1, 1, 1, 1, 1, 1, 1, 58,
+        58, 58, 58, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 58,
+        58, 58, 58, 58, 1, 1, 1, 2, 2, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 58,
+        58, 58, 58, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 58,
+        58, 58, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1, 57, 57, 58,
+        58, 58, 58, 1, 1, 1, 1, 1, 2, 2, 2, 2, 45, 2, 2, 2, 1, 57, 57, 58,
+        58, 58, 58, 58, 58, 58, 1, 1, 2, 45, 2, 23, 9, 24, 45, 1, 1, 57, 57, 58,
+        58, 58, 58, 58, 58, 58, 1, 1, 1, 41, 6, 26, 2, 41, 42, 1, 1, 1, 57, 58,
+        58, 58, 58, 58, 58, 58, 1, 1, 1, 1, 1, 5, 45, 1, 1, 1, 1, 1, 57, 58,
+        58, 58, 58, 58, 58, 58, 1, 1, 1, 1, 1, 41, 42, 1, 55, 55, 55, 55, 57, 58,
+        58, 58, 58, 58, 58, 58, 1, 1, 1, 1, 1, 1, 1, 1, 55, 55, 55, 56, 56, 58,
+        58, 58, 58, 58, 58, 58, 1, 1, 1, 1, 1, 1, 1, 1, 55, 55, 55, 56, 56, 58,
+        58, 58, 58, 58, 58, 58, 58, 58, 1, 1, 1, 1, 1, 55, 55, 55, 56, 56, 56, 58,
+        58, 58, 58, 58, 58, 58, 1, 58, 58, 1, 58, 58, 58, 58, 56, 56, 58, 58, 58, 58,
+        58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 56, 58, 58, 58, 58, 58,
+        58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58
+      }
+    },
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 20,
+      height = 20,
+      id = 2,
+      name = "houses",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 35, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 114, 0, 0, 0, 0, 114, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 114, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    },
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 20,
+      height = 20,
+      id = 3,
+      name = "people",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 70, 0, 0, 70, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 70, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    },
+    {
+      type = "objectgroup",
+      draworder = "topdown",
+      id = 4,
+      name = "Spawn Point",
+      class = "",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      objects = {
+        {
+          id = 1,
+          name = "Player",
+          type = "",
+          shape = "rectangle",
+          x = 575.724,
+          y = 319.846,
+          width = 64,
+          height = 64,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 2,
+          name = "Camera",
+          type = "",
+          shape = "rectangle",
+          x = 577,
+          y = 512,
+          width = 64,
+          height = 64,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        }
+      }
+    }
+  }
+}
